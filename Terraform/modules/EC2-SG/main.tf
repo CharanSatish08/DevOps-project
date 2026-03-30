8156fc79868166ba9d0f971f78f842242d9c2f20
Charan <charansatish08@gmail.com>
2026-03-30 13:03:10 -0500

    added sg

--- Terraform/modules/EC2-SG/main.tf
+++ Terraform/modules/EC2-SG/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  security_groups = [demo-sg]
+  security_groups = ["demo-sg"]
 
   tags = {
     Name = "${var.project_name}-instance"
