6c600b63c2ed892b47a4165a3990dd86eeaeea3c
Charan <charansatish08@gmail.com>
2026-04-03 05:40:08 -0500

    Fmt change

--- Terraform/instance-prod/backend.tf
+++ Terraform/instance-prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-1303"
-    key    = "instance.tfstate"
-    region = "us-east-1"
+    bucket       = "terraform-backend-1303"
+    key          = "instance.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
