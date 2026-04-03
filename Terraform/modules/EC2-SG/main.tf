0b4b720e547381482508d3c0a5741d3a944141c4
Charan <charansatish08@gmail.com>
2026-04-03 07:47:11 -0500

    added files

--- Terraform/modules/EC2-SG/main.tf
+++ Terraform/modules/EC2-SG/main.tf
@@ -7,4 +7,26 @@ resource "aws_instance" "instance" {
     Name = "${each.key}-instance"
   }
 }
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = module.VPC.vpc_id
 
+  tags = {
+    Name = "${var.project_name}-demo-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = var.from_port
+  ip_protocol       = "tcp"
+  to_port           = var.to_port
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
