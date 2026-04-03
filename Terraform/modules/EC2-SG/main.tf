898e486beaa4bb39a3ca450c9016856cc1be057d
Charan <charansatish08@gmail.com>
2026-04-03 05:33:12 -0500

    pointed to default VPC

--- Terraform/modules/EC2-SG/main.tf
+++ Terraform/modules/EC2-SG/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = "vpc-0754bd2ca3c3356c6"
+  #vpc_id      = module.VPC.vpc_id
 
   tags = {
     Name = "${var.project_name}-demo-sg"
