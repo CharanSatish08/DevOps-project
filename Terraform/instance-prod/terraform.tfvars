a0beeea83a4e76a04e236d03b4172ab717e82da3
Charan <charansatish08@gmail.com>
2026-04-10 06:33:45 -0500

    creating instances

--- Terraform/instance-prod/terraform.tfvars
+++ Terraform/instance-prod/terraform.tfvars
@@ -1,6 +1,6 @@
 region         = "us-east-1"
 project_name   = "instanx-prod-v1"
 ami_id         = "ami-0ec10929233384c7f"
-instance_type  = "t3.micro"
+instance_type  = "t2.micro"
 from_port      = 443
 to_port        = 443
--- Terraform/modules/EC2-SG/main.tf
+++ Terraform/modules/EC2-SG/main.tf
@@ -7,8 +7,9 @@ resource "aws_instance" "instance" {
   instance_type = var.instance_type
   security_groups = [aws_security_group.demo1-sg.name]
   key_name = "devops"
+  for_each = ['Jenkins-master', 'Jenkins Slave', 'ansible']
   tags = {
-    Name = "${var.project_name}-instance"
+    Name = "${each.key}-instance"
   }
 }
 resource "aws_security_group" "demo1-sg" {
